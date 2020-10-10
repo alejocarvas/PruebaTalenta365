17e5afd77a2684068cae5022ca14a36593036663
Alejandro Carvajal <alejocarvas@gmail.com>
2020-10-10 16:30:54 -0500

    Add Game component in frontend project

---
+++ three_inline_frontend/src/components/Game.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <input type="text" v-model="propName">
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        props: {
+            propName: {
+                type: String,
+                default: '',
+                required: true
+            },
+        },
+        methods: {
+            name() {
+                
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
