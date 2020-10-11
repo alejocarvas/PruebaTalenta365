d369c49e5c0ef8d57d8a80e6fdd76080c33946d2
Alejandro Carvajal <alejocarvas@gmail.com>
2020-10-11 16:06:30 -0500

    ESLint configuration and Prettier plugin configuration

--- three_inline_frontend/src/components/HelloWorld.vue
+++ three_inline_frontend/src/components/HelloWorld.vue
@@ -3,23 +3,35 @@
     <h1>{{ msg }}</h1>
     <p>
       Si deseas conocer las reglas del juego visita el siguiente link
-      <a href="https://es.wikipedia.org/wiki/Tres_en_l%C3%ADnea#:~:text=ficha%20al%20oponente.-,Reglas,recta%20o%20diagonal%20por%20s%C3%ADmbolo." target="_blank" rel="noopener">ver reglas del juego</a>.
+      <a
+        href="https://es.wikipedia.org/wiki/Tres_en_l%C3%ADnea#:~:text=ficha%20al%20oponente.-,Reglas,recta%20o%20diagonal%20por%20s%C3%ADmbolo."
+        target="_blank"
+        rel="noopener"
+        >ver reglas del juego</a
+      >.
     </p>
     <h3>Quiero jugar ya!</h3>
     <ul>
       <li><a href="" target="_blank" rel="noopener">Jugar</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">Historial</a></li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+          target="_blank"
+          rel="noopener"
+          >Historial</a
+        >
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
